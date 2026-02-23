a20529d7e9ca2818257aad821d35d29ba437e4d6
KaleemAfrozShaik <kaleemgamehacks@gmail.com>
2026-02-24 00:08:30 +0530

    Fixed some bugs

--- server/index.js
+++ server/index.js
@@ -23,13 +23,13 @@ app.post(
   stripeWebhook
 );
 
-app.use(express.json());
-app.use(cookieParser());
 app.use(cors({
   origin: process.env.CLIENT_URL || "http://localhost:5173",
   credentials: true,
   methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"]
 }));
+app.use(express.json());
+app.use(cookieParser());
 
 const PORT = process.env.PORT || 8000;
 
